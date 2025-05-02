c63a8be8a5b3e04a14a01bf41cc8d044e2582e82
Mohammad Imran <immu.engineer@gmail.com>
2025-05-02 13:50:18 +0600

    my first stypescript

---
+++ module1/src/1.5.ts
@@ -0,0 +1,11 @@
+const user: {
+    company?: string;
+    firstName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    firstName: "Moahmmad",
+    lastName: "Imran",
+    isMarried: true
+}
+
---
+++ module1/src/1.6.ts
@@ -0,0 +1,39 @@
+{// function add(num1: number,num2: number=10):number{
+//     return num1+num2
+// }
+// add(2,2)
+
+// const addArrow = (num1:number,num2:number):number => num1+num2
+
+// const poorUser = {
+//     name: "Imran",
+//     balance: 0,
+//     addBalance(balance: number):string{
+//         return  `my current balance is ${this.balance+ balance}`
+//     }
+// }
+
+
+// const array:number[] = [1,2,3]
+// const newArr:number[] = array.map((ele:number)=> ele*ele);
+
+// const user = {
+//     id:345,
+//     name: {
+//         firstName: 'Imran',
+//         middleName: 'janina',
+//         lastName: 'manina'
+//     },
+//     contactNo: '0459595594',
+//     address: 'uganda'
+// }
+
+
+// const {address, name: {middleName:midname}} = user;
+
+const myFamily = ['ammu','abbu','vai','wife','monica,cd'];
+
+const [,,,myWife,...rest] = myFamily;
+
+
+}
